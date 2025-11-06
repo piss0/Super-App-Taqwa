c3232aa3c9937c15d6cbeade065818cdc59b4c18
piss0 <hafizrisk425@gmail.com>
2025-11-06 09:04:31 +0700

    belajar carousel

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,7 +1,31 @@
-import 'package:flutter/material.dart';
+import 'dart:async'; //timer coutdown
+import 'package:flutter/material.dart'; //courosel slider
+import 'package:carousel_slider/carousel_slider.dart'; //ambil data
+import 'package:http/http.dart' as http; // ambil data API JSON
+import 'dart:convert'; //decode JSON
+import 'package:geolocator/geolocator.dart'; //GPS
+import 'package:geocoding/geocoding.dart'; //Konversi GPS
+import 'package:intl/intl.dart'; //formater number
+import 'package:permission_handler/permission_handler.dart'; // Izin handler
+import 'package:shared_preferences/shared_preferences.dart'; // cache lokal
+import 'package:string_similarity/string_similarity.dart'; //fuzyy match string
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final CarouselController _controller = CarouselController();
+  int _curentIndex = 0;
+
+  final posterList = const <String>[
+    'assets/images/ramadhan.jpg',
+    'assets/images/idul fitri.jpg',
+    'assets/images/idul adha.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +40,7 @@ class HomePage extends StatelessWidget {
                 height: 130,
                 decoration: BoxDecoration(
                   color: const Color.fromARGB(255, 47, 141, 133),
-                  borderRadius: BorderRadius.circular(15)
+                  borderRadius: BorderRadius.circular(15),
                 ),
                 child: SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
@@ -33,16 +57,17 @@ class HomePage extends StatelessWidget {
                                 Image.asset('assets/images/ic_menu_doa.png'),
                                 Text(
                                   'Doa & Zikir',
-                                style:  TextStyle(
-                                  fontFamily: 'poppinsRegular',
-                                  color: Colors.white,
-                                ),)
+                                  style: TextStyle(
+                                    fontFamily: 'poppinsRegular',
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ],
                             ),
                           ),
                         ),
                       ),
-                        InkWell(
+                      InkWell(
                         onTap: () {},
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
@@ -50,19 +75,22 @@ class HomePage extends StatelessWidget {
                             decoration: BoxDecoration(),
                             child: Column(
                               children: [
-                                Image.asset('assets/images/ic_menu_jadwal_sholat.png'),
+                                Image.asset(
+                                  'assets/images/ic_menu_jadwal_sholat.png',
+                                ),
                                 Text(
                                   'Jadwal Sholat',
-                                style:  TextStyle(
-                                  fontFamily: 'poppinsRegular',
-                                  color: Colors.white,
-                                ),)
+                                  style: TextStyle(
+                                    fontFamily: 'poppinsRegular',
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ],
                             ),
                           ),
                         ),
                       ),
-                        InkWell(
+                      InkWell(
                         onTap: () {},
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
@@ -70,19 +98,22 @@ class HomePage extends StatelessWidget {
                             decoration: BoxDecoration(),
                             child: Column(
                               children: [
-                                Image.asset('assets/images/ic_menu_video_kajian.png'),
+                                Image.asset(
+                                  'assets/images/ic_menu_video_kajian.png',
+                                ),
                                 Text(
                                   'Video kajian',
-                                style:  TextStyle(
-                                  fontFamily: 'poppinsRegular',
-                                  color: Colors.white,
-                                ),)
+                                  style: TextStyle(
+                                    fontFamily: 'poppinsRegular',
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ],
                             ),
                           ),
                         ),
                       ),
-                        InkWell(
+                      InkWell(
                         onTap: () {},
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
@@ -93,10 +124,11 @@ class HomePage extends StatelessWidget {
                                 Image.asset('assets/images/ic_menu_zakat.png'),
                                 Text(
                                   'zakat',
-                                style:  TextStyle(
-                                  fontFamily: 'poppinsRegular',
-                                  color: Colors.white,
-                                ),)
+                                  style: TextStyle(
+                                    fontFamily: 'poppinsRegular',
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ],
                             ),
                           ),
@@ -107,9 +139,26 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+            _buildCarouselSection(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCarouselSection() {
+    return Column(
+      children: [
+        const SizedBox(height: 20),
+        CarouselSlider.builder(
+          itemCount: posterList.length,
+          itemBuilder: (context, index, realindex) {
+            final poster = posterList[index];
+            return Container(child: Image.asset (poster));
+          },
+          options: CarouselOptions(),
+        ),
+      ],
     );
   }
 }
